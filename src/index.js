c4fc398a1d5dc1e602edbfc64b5317e6d0f3c129
Santiago Carmuega <santiago.carmuega@line64.com>
2016-11-11 16:54:17 -0300

    Remove legacy component from index

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import ContextPropagator from './components/ContextPropagator';
 import LandingCanvas from './components/LandingCanvas';
 import BigVideoBrick from './components/BigVideoBrick';
 import FeatureListBrick from './components/FeatureListBrick';
-import ReviewListBrick from './components/ReviewListBrick';
 import FooterBrick from './components/FooterBrick';
 import SquizMailBrick from './components/SquizMailBrick';
 import TitleBrick from './components/TitleBrick';
@@ -16,7 +15,6 @@ export {
   LandingCanvas,
   BigVideoBrick,
   FeatureListBrick,
-  ReviewListBrick,
   FooterBrick,
   SquizMailBrick,
   TitleBrick,
