26a1eaecf6d8fe0d2194bce3cf479c01e2b53407
manuel <manuel.otero@line64.com>
2017-01-16 14:19:12 -0300

    update-footer-brick

--- src/components/FooterBrick/styles.js
+++ src/components/FooterBrick/styles.js
@@ -1,78 +1,160 @@
-import { safeThemeFromProps, getBandStyle, lightenColor } from '../../utils/styleHelpers';
+import { safeThemeFromProps } from '../../utils/styleHelpers';
+
+function getWrapperForDesign(props) {
+
+  let {
+    textColor,
+    primaryColor,
+    primaryContrastColor,
+    objectDesign
+  } = safeThemeFromProps(props);
+
+  switch (objectDesign) {
+    case 'rounded-solid':
+      return {
+        backgroundColor: primaryColor,
+        color: primaryContrastColor,
+        borderStyle: 'none',
+        borderRadius: '30px'
+      };
+    case 'rounded-outline':
+      return {
+        backgroundColor: 'transparent',
+        borderWidth: 3,
+        borderColor: primaryColor,
+        color: primaryColor,
+        borderStyle: 'solid',
+        borderRadius: '30px'
+      };
+    case 'square-outline':
+      return {
+        backgroundColor: 'transparent',
+        borderWidth: 3,
+        borderColor: primaryColor,
+        color: primaryColor,
+        borderStyle: 'solid',
+        borderRadius: 0
+      };
+    default: //square-solid
+      return {
+        backgroundColor: primaryColor,
+        color: primaryContrastColor,
+        borderStyle: 'none',
+        borderRadius: 0
+      };
+  }
+
+}
 
 export default function (props) {
 
-  let theme = safeThemeFromProps(props);
+  let {
+    textColor,
+    primaryColor,
+    fontSize,
+    logoHeight
+  } = safeThemeFromProps(props);
 
   let {
-    background,
-    textColor,
-    primaryColor
-  } = getBandStyle(theme, props.bandStyle)
+    wrapperStyle,
+    iconStyle,
+    titleStyle,
+    descriptionStyle,
+    imageStyle
+  } = props;
 
-  return {
-    box: {
-      background: background,
-      color: textColor,
-      padding: '10%',
-      paddingTop: '2%',
-      paddingBottom: '2%',
-      textAlign: 'center'
-    },
-    container: {
-       display: 'flex'
-    },
-    noList: {
-      listStyle: 'none'
-    },
-    text: {
-      textDecoration: 'none',
-      fontSize: '1rem',
-      color: textColor
-    },
-    textColumn1 : {
+  let styles = {
+    wrapper: {
+      ...getWrapperForDesign(props),
+      textAlign: 'center',
+      minHeight: '50px',
+      padding : '2% 8%',
+      marginTop: '-10px',
       flex: 1,
-      textAlign: 'left'
-    },
-    textColumn2 : {
-      flex: 1,
-      textAlign: 'justify'
-    },
-    textColumn3 : {
-      flex: 1,
-      textAlign: 'right'
+      ...wrapperStyle,
     },
     icon: {
-      fontSize: '1.5rem',
-      margin: '5px',
-      width:45,
-      textAlign: 'left'
+      fontSize: '2em',
+      ...iconStyle
     },
-    simpleList: {
-      margin: 0,
+    image: {
+      width: '6em',
+      ...imageStyle
+    },
+    title: {
+      fontSize: '1em',
+      fontWeight: 'bold',
+      margin: '0.5%',
+      color: textColor,
+      ...titleStyle
+    },
+    description: {
+      fontSize: '0.8em',
+      fontWeight: 'normal',
+      margin: '0.5%',
+      color: textColor,
+      ...descriptionStyle
+    },
+    center: {
+      textAlign: 'center',
+      alignSelf: 'center',
+      display: 'flex',
+      justifyContent: 'center'
+
+    },
+    noLink: {
+      textDecoration: 'none',
+      color: textColor,
+      fontSize: fontSize
+    },
+    item: {
+      flex : 1,
+      color: textColor,
+      fontSize: fontSize
+    },
+    leftContainter: {
+      flex: 4,
+      textAlign: 'left',
       listStyle: 'none',
-      textAlign: 'left'
+      display: 'flex',
+      flexDirection: 'column',
+      margin: 0
     },
-    simpleListItem: {
-      display: 'inline-block',
-      textAlign: 'left',
-      fontSize: '1rem',
-      padding: '0 12px',
-      borderRight: '2px solid #FFF',
-      lineHeight: '1rem'
+    rightContainter: {
+      flex: 1,
+      paddingRight: 25,
+      textAlign: 'right',
+      listStyle: 'none',
+      display: 'flex',
+      flexDirection: 'column',
+      alignSelf: "center",
+      margin: 0
     },
-    simpleCopy: {
-      textAlign: 'left',
-      fontSize: '1rem',
-      marginTop: '-4px'
+    enterprise: {
+      color: textColor,
+      fontSize: '1.2rem',
+      margin: 0,
+      fontWeight: '300',
+      marginTop: 5
     },
-    simpleLogo: {
-      position: 'relative',
-      marginLeft: '50px',
-      marginRight: '2px',
-      top: '10px',
-      height: '40px'
+    icon: {
+      borderRadius: '50%',
+      border: '2px solid' + textColor,
+      color: textColor,
+      padding: 4,
+      textDecoration: 'none'
+    },
+    Faicon: {
+      height: 30,
+      width: 25,
+      textAlign: 'center'
+    },
+    logo: {
+      height: logoHeight || '25px',
+      verticalAlign : 'middle'
     }
   };
+
+  return styles;
 
 }
